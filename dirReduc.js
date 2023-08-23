3d06f3eeecb5177a8b68dd42811824cea6851ed8
Max Shabalin <shabalinmaksimromanovich@gmail.com>
2023-08-23 21:50:21 +0300

    codewars to 1 key, day 1

--- dirReduc.js
+++ dirReduc.js
@@ -1,31 +1,9 @@
-
-function dirReduc(arr){
+function dirReduc(arr) {
     const map = {
         "SOUTH": "NORTH",
-        "NORTH": "SOUTH" ,
-        "WEST": "EAST" ,
-        "EAST": "WEST" ,
+        "NORTH": "SOUTH",
+        "WEST": "EAST",
+        "EAST": "WEST",
     }
-    const isNextFake = (subArr) => {
-        if (map[subArr[0]] === subArr[1]) {
-            return []
-        } else {
-            return subArr;
-        }
-    }
-    const res = [];
-    for (let i = 0; i < arr.length ; i++) {
-        if (i === arr.length - 1) {
-            continue;
-        }
-        const isNextFakeData = isNextFake([arr[i], arr[i + 1]])
-        if (isNextFakeData.length === 0) {
-            i += 2
-        } else {
-            res.push(...isNextFakeData);
-        }
-    }
-    return res;
+    // todo
 }
-
-console.log(dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"]))
